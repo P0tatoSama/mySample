e95918e95facce4478bf22b2d2b90e130f65a7c0
taruc <taruc@B6>
2019-11-13 10:53:51 +0800

    [ Finishes #169722047 ]

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -17,6 +17,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
        System.out.println("Hello,World");
        System.out.println("Branch 1");
+       System.out.println("naniii");
     }
     
 }
